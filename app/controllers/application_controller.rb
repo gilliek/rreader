592709611f85426baccc8487eaf578a8dc47292c
gilliek <kevin.gillieron@gw-computing.net>
2013-05-14 00:36:10 +0200

    really fixed the redirect loop !

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :check_first_use
   before_filter :authenticate_user!
   before_filter :streams_list
-  before_filter :check_first_use
 
   private
     def streams_list
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
     end
 
     def check_first_use
-      if params[:action] != 'sign_up'
+      if params[:action] != 'new' && params[:controller] != 'devise/sessions'
         num_users = User.all.count
         redirect_to new_user_registration_url if num_users == 0
       end
