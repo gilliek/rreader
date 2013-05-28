566beb0a2c2782663c0c6b3dc11518a359fef1aa
Robin Hahling <robin.hahling@gw-computing.net>
2013-05-28 17:15:20 +0200

    fixed error message in signup page

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,19 @@
+module DeviseHelper
+	# Override default devise error message
+	def devise_error_messages!
+		return "" if resource.errors.empty?
+
+		messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+		sentence = I18n.t("errors.messages.not_saved",
+						  :count => resource.errors.count,
+						  :resource => resource.class.model_name.human.downcase)
+
+		html = <<-HTML
+		<div class="alert alert-error">
+		  <ul>#{messages}</ul>
+		</div>
+		HTML
+
+		html.html_safe
+	end
+end
